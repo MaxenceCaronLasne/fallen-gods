787acefdc1cd852ad42c59366606a97eab85b9db
Maxence Caron-Lasne <maxence.caron-lasne@epita.fr>
2024-01-31 14:54:11 +0100

    fix: change jump values

--- move_component.gd
+++ move_component.gd
@@ -1,13 +1,31 @@
 extends Node
 
+enum MoveType {
+	Fix,
+	Slippy,
+}
+
 @export var _actor: CharacterBody2D
 @export var _speed: float
 @export var _max_speed: float
+@export var _move_type: MoveType
 
 func _ready():
 	assert(_actor)
 
+func _get_fixed_velocity(direction: float, delta: float) -> float:
+	return direction * _max_speed * delta
+
+func _get_slippy_velocity(direction: float, initial_velocity: float, delta: float) -> float:
+	var res := move_toward(
+		initial_velocity,
+		direction * _max_speed, 
+		_speed * delta)
+	return res
+
 func _process(delta: float):
 	var direction := Input.get_axis("left", "right")
-	_actor.velocity.x = move_toward(
-		_actor.velocity.x, direction * _max_speed, _speed * delta)
+	
+	match _move_type:
+		MoveType.Fix: _actor.velocity.x = _get_fixed_velocity(direction, delta)
+		MoveType.Slippy: _actor.velocity.x = _get_slippy_velocity(direction, _actor.velocity.x, delta)
