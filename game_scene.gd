8f18df2a0eb46509b690b824ad20d49a1ac9b272
Maxence Caron-Lasne <maxence.caron-lasne@epita.fr>
2024-02-01 19:18:47 +0100

    add: reset level animation

--- game_scene.gd
+++ game_scene.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+@onready var _animation_player := $AnimationPlayer as AnimationPlayer
+@onready var _shake_component := $ShakeComponent as ShakeComponent
+
+var _is_reseting: bool = false
+
 func _ready():
-	pass
+	_animation_player.play("open_door")
 
 func _process(_delta):
 	pass
 
 func _on_player_just_died():
-	await get_tree().create_timer(0.3).timeout
-	get_tree().change_scene_to_file("res://game_scene.tscn")
+	_animation_player.play("close_door")
+
+func _on_animation_player_animation_finished(anim_name: StringName):
+	if anim_name == "close_door":
+		_shake_component.shake(2.0)
+		_is_reseting = true
+
+func _on_shake_component_ended_shaking():
+	if _is_reseting:
+		get_tree().change_scene_to_file("res://game_scene.tscn")
---
+++ shake_component.gd
@@ -0,0 +1,41 @@
+extends Node
+class_name ShakeComponent
+
+signal ended_shaking
+
+const NOISE_PICKER_SPEED: float = 500.0
+const NOISE_AMP: float = 4.0
+const SHAKE_RECOVERY_RATE: float = 5.0
+
+@export var _actor: Node2D = null
+
+var _initial_position: Vector2 = Vector2.ZERO
+var _shake_factor: float = 0.0
+
+var _noise_offset: float = 0.0
+var _is_shaking: bool = false
+
+@onready var _noise := FastNoiseLite.new()
+
+func shake(intensity: float) -> void:
+	_shake_factor += intensity
+	_is_shaking = true
+
+func _ready():
+	assert(_actor)
+	_initial_position = _actor.position
+
+func _process(delta: float):
+	if is_zero_approx(_shake_factor):
+		if _is_shaking:
+			_is_shaking = false
+			ended_shaking.emit()
+		return
+	
+	var offset_x := _noise.get_noise_2d(0, _noise_offset) * _shake_factor
+	var offset_y := _noise.get_noise_2d(10, _noise_offset) * _shake_factor
+	_noise_offset += NOISE_PICKER_SPEED * delta
+	
+	_actor.position = _initial_position + Vector2(offset_x, offset_y)
+	
+	_shake_factor = clampf(_shake_factor - (SHAKE_RECOVERY_RATE * delta), 0.0, INF)
