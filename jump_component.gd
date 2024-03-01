f53f0fe3d331b3057871a536447ad220229674be
Maxence Caron-Lasne <maxence.caron-lasne@epita.fr>
2024-03-01 17:19:37 +0100

    add: configurable nb of double jumps

--- jump_component.gd
+++ jump_component.gd
@@ -11,10 +11,13 @@ enum JumpType {NONE, FROM_GROUND, FROM_AIR}
 @export var _input_jump: String = "jump"
 @export var _jump_settings: JumpSettings = null
 @export var _lateral_speed: float = 0.0
+@export var _nb_of_double_jumps := 2
 
 var _current_jump_type: JumpType = JumpType.NONE
 var _is_accepting_input: bool = false
 var _was_falling_last_frame: bool = false
+
+@onready var _current_nb_double_jump := _nb_of_double_jumps
 
 func accept_input() -> void:
 	_is_accepting_input = true
@@ -51,19 +54,34 @@ func _process_lateral_move() -> void:
 
 	_actor.velocity.x = direction * _lateral_speed
 
-func _process_jump(_delta: float) -> void:
-	if Input.is_action_just_pressed(_input_jump) and _current_jump_type == JumpType.NONE and _is_accepting_input:
-		_actor.velocity.y = _jump_settings.get_velocity()
-		_current_jump_type = JumpType.FROM_GROUND
-		jump_from_ground.emit()
-	elif Input.is_action_just_pressed(_input_jump) and _current_jump_type == JumpType.FROM_GROUND and _is_accepting_input:
-		var is_double_jump := true
-		_actor.velocity.y = _jump_settings.get_velocity(is_double_jump)
-		_current_jump_type = JumpType.FROM_AIR
-		jump_from_air.emit()
+func _process_ground_jump() -> void:
+	_actor.velocity.y = _jump_settings.get_velocity()
+	_current_jump_type = JumpType.FROM_GROUND
+	jump_from_ground.emit()
 
-	if Input.is_action_just_released(_input_jump) and (_current_jump_type == JumpType.FROM_GROUND or _current_jump_type == JumpType.FROM_AIR) and not is_falling() and _is_accepting_input:
-		_actor.velocity.y *= _jump_settings.get_jump_brake()
+func _process_air_jump() -> void:
+	if _current_nb_double_jump <= 0:
+		return
+
+	var is_double_jump := true
+	_actor.velocity.y = _jump_settings.get_velocity(is_double_jump)
+	_current_jump_type = JumpType.FROM_AIR
+	jump_from_air.emit()
+	_current_nb_double_jump -= 1
+
+func _process_jump(_delta: float) -> void:
+	if not _is_accepting_input:
+		return
+
+	if Input.is_action_just_pressed(_input_jump):
+		match _current_jump_type:
+			JumpType.NONE: _process_ground_jump()
+			JumpType.FROM_GROUND, JumpType.FROM_AIR: _process_air_jump()
+
+	if Input.is_action_just_released(_input_jump) and not is_falling():
+		match _current_jump_type:
+			JumpType.FROM_GROUND, JumpType.FROM_AIR:
+				_actor.velocity.y *= _jump_settings.get_jump_brake()
 
 func _physics_process(delta: float):
 	_process_gravity(delta)
@@ -73,3 +91,4 @@ func _physics_process(delta: float):
 
 func _on_touched_floor() -> void:
 	_current_jump_type = JumpType.NONE
+	_current_nb_double_jump = _nb_of_double_jumps
