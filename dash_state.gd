8f7141960f15551347ab2222e0e0092c384cf4a8
Maxence Caron-Lasne <maxence.caron-lasne@epita.fr>
2024-03-04 18:36:49 +0100

    add: draft dash mechanisms

--- dash_state.gd
+++ dash_state.gd
@@ -1,11 +1,29 @@
 extends MovementState
 
+enum State {
+	NotDashing,
+	Dashing,
+}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var _duration: float = 1.0
 
+var _current_state := State.NotDashing
+var _direction := Vector2.ZERO
+var _speed := 40.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func enter() -> void:
+	_actor.velocity = Vector2.ZERO
+	_current_state = State.Dashing
+	
+	_direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
+	_actor.velocity = _direction * _speed
+	
+	await get_tree().create_timer(_duration).timeout
+	_current_state = State.NotDashing
+	exited.emit(MovementStateMachine.State.Move)
+
 func _process(delta):
-	pass
+	if _current_state == State.NotDashing:
+		return
+	
+	
